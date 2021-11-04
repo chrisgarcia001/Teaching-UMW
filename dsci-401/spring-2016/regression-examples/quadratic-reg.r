8c08dad1b0c2bd2f5e90d170931bfe6b320680a4
Chris Garcia <chrisgarcia001@gmail.com>
2021-11-04 12:28:47 -0400

    Update code example for better teaching clarity

--- dsci-401/spring-2016/regression-examples/quadratic-reg.r
+++ dsci-401/spring-2016/regression-examples/quadratic-reg.r
@@ -1,23 +1,33 @@
- rd <- read.csv("batting-angles-distances.csv")
- rd
- 
- # Build quadratic regression points for each angle, based on formula: Distance ~ Angle*Angle + Angle
- # I(rd$Angle^2) creates a quadratic variable (Angle x Angle)
- # Can also do poly(rd
- yvals <- predict(lm(rd$Distance ~ I(rd$Angle^2) + rd$Angle), newdata=data.frame(Angle=rd$Angle))
- 
- #Another way using poly to create a polynomial for Angle of maximum degree 2.
- yvals <- predict(lm(rd$Distance ~ poly(rd$Angle, 2)), newdata=data.frame(Angle=rd$Angle))
- 
- plot(rd$Angle, rd$Distance, col="blue", pch=19)
- lines(rd$Angle, yvals, col="red")
- 
- #------------ Manual way
- rd$AngleSq <- rd$Angle^2
- yvals <- predict(lm(rd$Distance ~ rd$AngleSq + rd$Angle), newdata=data.frame(Angle=rd$Angle))
- plot(rd$Angle, rd$Distance, col="blue", pch=19)
- lines(rd$Angle, yvals, col="red") 
- 
- 
- 
- 
+# Illustrate how to deal with cases where the data is clearly not linear
+rd <- read.csv('batting-angles-distances.csv')
+head(rd)
+
+# Put data in order by Angle to ensure correct trend line drawing
+rd <- rd[order(rd$Angle), ]
+head(rd) # Notice it is in order now
+
+# Look at the data
+plot(rd$Angle, rd$Distance, col="blue", pch=19)
+
+# Build a linear fit to see how it does
+linear.mod <- lm(Distance ~ Angle, data=rd)
+summary(linear.mod)
+linear.preds <- predict(linear.mod, newdata=rd)
+lines(rd$Angle, linear.preds, col="black", lwd=2)
+
+# Add an Angle^2 column
+rd$AngleSq <- rd$Angle^2
+head(rd)
+
+# Build our quadratic model
+quad.mod <- lm(Distance ~ AngleSq + Angle, data=rd)
+
+# Look at the summary
+summary(quad.mod)
+
+# Make predictions based on quadratic fit model
+quad.preds <- predict(quad.mod, newdata=rd)
+
+# Plot the regression line
+lines(rd$Angle, quad.preds, col="red", lwd=2)
+
